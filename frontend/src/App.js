c81ebc5e10cf02ad53aef8cc16fbb3ef5d3c0482
Mahesh Sathe <123986496+maheshsathe07@users.noreply.github.com>
2024-11-24 13:00:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ExtractData from "./components/ExtractData";
 import ExtractSchema from "./components/ExtractSchema";
 import GenerateInsights from "./components/GenerateInsights";
+import GenerateSQLInsights from "./components/GenerateSQLInsights";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";  // Import the new Home component
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />  {/* New Home page route */}
           <Route path="/extract-data" element={<ExtractData />} />
           <Route path="/extract-schema" element={<ExtractSchema />} />
           <Route path="/generate-insights" element={<GenerateInsights />} />
+          <Route path="/generate-sql-insights" element={<GenerateSQLInsights />} />
         </Routes>
       </div>
     </Router>
